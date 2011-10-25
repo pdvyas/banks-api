2a961b9a719ca5056ba96fafd9ee6d0343764a8b
Pratik Vyas <pdvyas@erpnext.com>
2011-10-25 22:33:28 +0530

    Login works

--- hdfc.py
+++ hdfc.py
@@ -1,5 +1,6 @@
 import mechanize
 import cookielib
+from BeautifulSoup import BeautifulSoup
 
 class hdfc:
 	def __init__(self,cust_id):
@@ -26,18 +27,42 @@ class hdfc:
 		self.br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 		self.state = "initiated"
 
-	def start(self,cust_id):
+	def start(self):
 		self.br.open('https://m.netbanking.hdfcbank.com/netbanking/')
 		self.br.select_form(nr=0)
 		self.br.form['fldLoginUserId'] = self.cust_id
 		self.br.submit()
 		self.state = "started"
-	
+		html = self.br.response().read()
+		soup = 	BeautifulSoup(html)
+		# Return authentication info
+		return {
+		'phrase':soup.findAll('input',{'name':'fldRSAUserPhrase'})[0]['value'],
+		'image':soup.findAll('img')[1]['src']
+		}
+
 	def login(self,password):
+		self.br.select_form('frmLogon')
+		self.br.form['fldPassword']=password
+		self.br.find_control('fldCheck').items[0].selected=True
+		self.br.submit()
+		print self.br.response().read()
 		self.state = "logged in"
 	
 	def get_accounts(self):
 		pass
 	
 	def get_account_statement(self,ac_no):
+		pass
+	
+	def logout(self):
+		self.br.select_form('frmlogoff')
+		self.br.submit()
+
+		pass
+	
+	def parse_accounts(self,html):
+		pass
+	
+	def parse_account_statement(self,html):
 		pass
